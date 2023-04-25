d790d2911ff5c46c4b40f6ce1c691b15345c7caa
Artem <a.yakimov05@gmail.com>
2023-04-25 16:35:09 +0200

    Added auto login, that works by using client id. Every new registered player gets client id.

--- game_resources.py
+++ game_resources.py
@@ -4,6 +4,33 @@ from flask_restful import Resource
 from game_db import *
 
 
+def card_params_list(params, ses):
+    name = params.name
+    bp = params.bp
+    armor = params.armor
+    provision = params.provision
+    card_type = params.card_type
+    fraction = params.fraction
+    tags = params.tags
+    if ses.query(Methods).filter(Methods.id == params.deployment).first():
+        deployment = ses.query(Methods).filter(Methods.id == params.deployment).first().name
+    else:
+        deployment = None
+    if ses.query(Methods).filter(Methods.id == params.order).first():
+        order = ses.query(Methods).filter(Methods.id == params.order).first().name
+    else:
+        order = None
+    if ses.query(Methods).filter(Methods.id == params.turn_end).first():
+        turn_end = ses.query(Methods).filter(Methods.id == params.turn_end).first().name
+    else:
+        turn_end = None
+    if ses.query(Methods).filter(Methods.id == params.conditional).first():
+        conditional = ses.query(Methods).filter(Methods.id == params.conditional).first().name
+    else:
+        conditional = None
+    return [name, bp, armor, provision, card_type, fraction, deployment, order, turn_end, conditional, tags]
+
+
 class DeckResource(Resource):
     def create_deck(self, cards_ids):
         global_init("cards")
@@ -11,30 +38,7 @@ class DeckResource(Resource):
         cards = []
         for card_id in [int(i) for i in cards_ids.split(";")]:
             params = session.query(CardsParams).filter(CardsParams.id == card_id).first()
-            name = params.name
-            bp = params.bp
-            armor = params.armor
-            provision = params.provision
-            card_type = params.card_type
-            fraction = params.fraction
-            tags = params.tags
-            if session.query(Methods).filter(Methods.id == params.deployment).first():
-                deployment = session.query(Methods).filter(Methods.id == params.deployment).first().name
-            else:
-                deployment = None
-            if session.query(Methods).filter(Methods.id == params.order).first():
-                order = session.query(Methods).filter(Methods.id == params.order).first().name
-            else:
-                order = None
-            if session.query(Methods).filter(Methods.id == params.turn_end).first():
-                turn_end = session.query(Methods).filter(Methods.id == params.turn_end).first().name
-            else:
-                turn_end = None
-            if session.query(Methods).filter(Methods.id == params.conditional).first():
-                conditional = session.query(Methods).filter(Methods.id == params.conditional).first().name
-            else:
-                conditional = None
-            cards.append([name, bp, armor, provision, card_type, fraction, deployment, order, turn_end, conditional, tags])
+            cards.append(card_params_list(params, session))
         return cards
 
     def get(self, deck_cards):
@@ -47,31 +51,7 @@ class CardsResource(Resource):
         session = create_session()
         cards = {}
         for params in session.query(CardsParams).all():
-            # params = session.query(CardsParams).filter(CardsParams.id == card_id).first()
-            name = params.name
-            bp = params.bp
-            armor = params.armor
-            provision = params.provision
-            card_type = params.card_type
-            fraction = params.fraction
-            tags = params.tags
-            if session.query(Methods).filter(Methods.id == params.deployment).first():
-                deployment = session.query(Methods).filter(Methods.id == params.deployment).first().name
-            else:
-                deployment = None
-            if session.query(Methods).filter(Methods.id == params.order).first():
-                order = session.query(Methods).filter(Methods.id == params.order).first().name
-            else:
-                order = None
-            if session.query(Methods).filter(Methods.id == params.turn_end).first():
-                turn_end = session.query(Methods).filter(Methods.id == params.turn_end).first().name
-            else:
-                turn_end = None
-            if session.query(Methods).filter(Methods.id == params.conditional).first():
-                conditional = session.query(Methods).filter(Methods.id == params.conditional).first().name
-            else:
-                conditional = None
-            cards[name] = [name, bp, armor, provision, card_type, fraction, deployment, order, turn_end, conditional, tags]
+            cards[params.name] = card_params_list(params, session)
         return cards
 
     def get(self):
@@ -79,19 +59,28 @@ class CardsResource(Resource):
 
 
 class PlayerResource(Resource):
-    def get(self, email, password):
+    game_version = None
+
+    def get(self, email, password, cid, conn_type):
         global_init("users")
         session = create_session()
-        player = session.query(User).filter(User.email == email).first()
+        if conn_type == "in_email":
+            player = session.query(User).filter(User.email == email).first()
+        elif conn_type == "in_cid":
+            player = session.query(User).filter(User.client_id == int(cid)).first()
         if player:
-            if player.game_version != "0.01":
-                return jsonify(f"Версия игры должна быть 0.01. Ваша {player.game_version}")
+            if player.game_version != self.game_version:
+                return jsonify(f"Версия игры должна быть {self.game_version}. Ваша {player.game_version}")
+            if conn_type == "in_cid":
+                player.online = 1
+                session.commit()
+                return jsonify([player.id, player.nickname, player.client_id])
             if player.check_password(password):
-                if player.online == 0:
+                if conn_type == "in_email":
                     player.online = 1
                     session.commit()
-                    return jsonify([player.id, player.nickname])
-                else:
+                    return jsonify([player.id, player.nickname, player.client_id])
+                elif conn_type == "out":
                     player.online = 0
                     session.commit()
                     return
--- website.py
+++ website.py
@@ -7,16 +7,17 @@ from flask_restful import reqparse, abort, Api, Resource
 
 
 from game_resources import *
-
-server = "192.168.0.195"
+# server = "192.168.0.195"
+server = "127.0.0.1"
 port = 8080
-
+VERSION = "0.01"
 site = Flask(__name__)
 api = Api(site)
 site.config["SECRET_KEY"] = "Github"
 log_mg = LoginManager()
 log_mg.init_app(site)
-
+PlayerResource.game_version = VERSION
+USERS_ONLINE = {}
 
 class RegistrationForm(FlaskForm):
     email = EmailField('Почта', validators=[DataRequired()])
@@ -43,6 +44,7 @@ def front_page():
 
 @site.route('/register', methods=['GET', 'POST'])
 def reqister():
+    import random
     form = RegistrationForm()
     if form.validate_on_submit():
         if form.password.data != form.password_again.data:
@@ -59,10 +61,16 @@ def reqister():
             return render_template('register.html', title='Регистрация',
                                    form=form,
                                    message="Такой никнейм уже занят")
+        all_used_client_id = [user.client_id for user in session.query(User).all()]
+        print(all_used_client_id)
+        new_client_id = random.randint(1_010_010, 9_909_900)
+        while new_client_id not in all_used_client_id:
+            new_client_id = random.randint(1_010_010, 9_909_900)
         user = User(
             nickname=form.nickname.data,
             email=form.email.data,
-            hashed_password=form.password_again.data
+            hashed_password=form.password_again.data,
+            client_id=new_client_id
         )
         user.set_password(form.password.data)
         session.add(user)
@@ -93,6 +101,12 @@ def login():
     return render_template('login.html', title='Авторизация', form=form)
 
 
+@site.route('/connection/<id>')
+def check_connection(id):
+    # global_init("users")
+    # session = create_session()
+    USERS_ONLINE[id] = 0
+
 @site.route('/logout')
 @login_required
 def logout():
@@ -116,6 +130,6 @@ if __name__ == '__main__':
 
     api.add_resource(DeckResource, '/api/v2/my_deck/<deck_cards>')
     api.add_resource(CardsResource, '/api/v2/my_deck/constructor')
-    api.add_resource(PlayerResource, '/api/v2/player/<email>&<password>')
+    api.add_resource(PlayerResource, '/api/v2/player/<email>&<password>&<cid>&<conn_type>')
 
     site.run(port=port, host=server)
